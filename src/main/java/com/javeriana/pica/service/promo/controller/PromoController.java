9b505cb8f7cdd1bdddc508a5e0133d568af38944
Alfonso <alfonsocarop@gmail.com>
2019-11-21 22:09:31 -0500

    validate JWT

--- src/main/java/com/javeriana/pica/service/promo/controller/PromoController.java
+++ src/main/java/com/javeriana/pica/service/promo/controller/PromoController.java
@@ -6,9 +6,15 @@ import com.javeriana.pica.service.promo.repository.PromoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.Optional;
+
+import com.javeriana.pica.service.promo.security.Validate_Token;
 
 @RestController
 public class PromoController {
@@ -18,12 +24,22 @@ public class PromoController {
 
     @GetMapping("/promos")
     public Page<Promo> getQuestions(Pageable pageable) {
+
         return promoRepository.findAll(pageable);
     }
 
     @PostMapping("/promos")
-    public Promo createQuestion(@Valid @RequestBody Promo promo) {
-        return promoRepository.save(promo);
+    public ResponseEntity<?> createQuestion(
+            @Valid @RequestBody Promo promo,
+            @RequestHeader (name="Authorization") String token
+            )
+            throws Exception {
+        Validate_Token validate_token = new Validate_Token();
+        if(validate_token.send_token(token)){
+            promoRepository.save(promo);
+            return new ResponseEntity<>(promo, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>("{'error': 'UNAUTHORIZED'}", HttpStatus.UNAUTHORIZED);
     }
 
     @GetMapping("/promos/{promoId}")
@@ -32,27 +48,45 @@ public class PromoController {
     }
 
     @PutMapping("/promos/{promoId}")
-    public Promo updatePromo(@PathVariable Long promoId,
-                                   @Valid @RequestBody Promo promoRequest) {
-        return promoRepository.findById(promoId)
-                .map(promo -> {
-                    promo.setName(promoRequest.getName());
-                    promo.setImage_url(promoRequest.getImage_url());
-                    promo.setId_product(promoRequest.getId_product());
-                    promo.setInitial_date(promoRequest.getInitial_date());
-                    promo.setEnd_date(promoRequest.getEnd_date());
-                    promo.setDescription(promoRequest.getDescription());
-                    promo.setEstate(true);
-                    return promoRepository.save(promo);
-                }).orElseThrow(() -> new ResourceNotFoundException("Promo not found with id " + promoId));
+    public ResponseEntity<?>  updatePromo(@PathVariable Long promoId,
+                             @Valid @RequestBody Promo promoRequest,
+                             @RequestHeader (name="Authorization") String token
+    ) throws Exception{
+        Validate_Token validate_token = new Validate_Token();
+        if(validate_token.send_token(token)){
+            Promo promo_r = promoRepository.findById(promoId)
+                    .map(promo -> {
+                        promo.setName(promoRequest.getName());
+                        promo.setImage_url(promoRequest.getImage_url());
+                        promo.setId_product(promoRequest.getId_product());
+                        promo.setInitial_date(promoRequest.getInitial_date());
+                        promo.setEnd_date(promoRequest.getEnd_date());
+                        promo.setDescription(promoRequest.getDescription());
+                        promo.setEstate(true);
+                        return promoRepository.save(promo);
+                    }).orElseThrow(() -> new ResourceNotFoundException("Promo not found with id " + promoId));
+            return new ResponseEntity<>(promo_r, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("{error: UNAUTHORIZED}", HttpStatus.UNAUTHORIZED);
+
     }
 
     @DeleteMapping("/promos/{promoId}")
-    public Promo deletePromo(@PathVariable Long promoId) {
-        return promoRepository.findById(promoId)
-                .map(promo -> {
-                    promo.setEstate(false);
-                    return promoRepository.save(promo);
-                }).orElseThrow(() -> new ResourceNotFoundException("Question not found with id " + promoId));
+    public ResponseEntity<?> deletePromo(
+            @PathVariable Long promoId,
+            @RequestHeader (name="Authorization") String token
+    ) throws Exception {
+        Validate_Token validate_token = new Validate_Token();
+        if (validate_token.send_token(token)) {
+            Promo promo_r = promoRepository.findById(promoId)
+                    .map(promo -> {
+                        promo.setEstate(false);
+                        return promoRepository.save(promo);
+                    }).orElseThrow(() -> new ResourceNotFoundException("Question not found with id " + promoId));
+
+            return new ResponseEntity<>(promo_r, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("{error: UNAUTHORIZED}", HttpStatus.UNAUTHORIZED);
     }
+
 }
---
+++ src/main/java/com/javeriana/pica/service/promo/security/Validate_Token.java
@@ -0,0 +1,52 @@
+package com.javeriana.pica.service.promo.security;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
+
+public class Validate_Token {
+
+    public boolean send_token(String token) throws Exception {
+        String url = "http://13.90.244.183/validate/";
+        return requestHttpGet(url, token);
+    }
+
+    public static Boolean requestHttpGet(String url_to_validate, String token) throws Exception {
+        URL url = new URL(url_to_validate);
+
+        HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+        connect.setRequestProperty("Content-Type", "application/json");
+        connect.setRequestProperty("Accept", "application/json");
+        connect.setRequestProperty("authorization", token);
+        connect.setRequestMethod("GET");
+
+        try{
+            if (connect.getResponseCode() == 401){
+                return false;
+            }else if (connect.getResponseCode() == 200) {
+
+                try (BufferedReader br = new BufferedReader(
+                        new InputStreamReader(connect.getInputStream(), "utf-8"))) {
+                    StringBuilder response = new StringBuilder();
+                    String responseLine = null;
+                    while ((responseLine = br.readLine()) != null) {
+                        response.append(responseLine.trim());
+                    }
+
+                    JSONObject myResponse = new JSONObject(response.toString());
+
+                    if (myResponse.getBoolean("is_staff")){
+                        return true;
+                    }
+                }
+            }
+        }catch (Exception e) {
+            // Manejar excepción
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
